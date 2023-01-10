b076677737c2037e73ca5ed60165379ce4ae145f
Prajwal Shrestha <prajwal08shrestha@gmail.com>
2023-01-10 15:38:25 +0545

    First collab

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,10 @@
 const inputValue = document.getElementById("value");
 const button = document.getElementById("btn");
 
-let sum = 0;
+let sum = 0;0
 let num;
+
+let name = prompt("Enter your name: ");
 
 function sumN(num) {
     for (let i = 0; i <= num; i++) {
@@ -13,5 +15,5 @@ function sumN(num) {
 document.addEventListener("submit", () => {
     num = inputValue.value;
     sumN(num);
-    alert(`The sum of first ${num} natural number is ${sum}.`);
+    alert(`${name}, The sum of first ${num} natural number is ${sum}.`);
 });
